dcad3f33ab72862e3711786902798bb626b71566
Lee Breisacher <leebreisacher@gmail.com>
2013-10-27 13:58:35 -0700

    choose first location

--- src/main/java/org/openmrs/uitestframework/page/LoginPage.java
+++ src/main/java/org/openmrs/uitestframework/page/LoginPage.java
@@ -2,6 +2,7 @@ package org.openmrs.uitestframework.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends AbstractBasePage {
 	
@@ -9,7 +10,7 @@ public class LoginPage extends AbstractBasePage {
 	static final String PASSWORD_TEXTBOX_ID = "password";
 	static final String LOGIN_BUTTON_ID = "login-button";
 	public static final String LOGIN_PATH = "/login.htm";
-    public static final String UNKOWN_LOCATION_ELEMENT_ID = "Registration Desk";
+    public static final String LOCATIONS_ID = "sessionLocation";
 	static final String LOGOUT_PATH = "/logout";
 	static final String CLERK_USERNAME = "clerk";
 	static final String CLERK_PASSWORD = "Clerk123";
@@ -31,7 +32,9 @@ public class LoginPage extends AbstractBasePage {
 	public void login(String user, String password) {
 		setTextToFieldNoEnter(By.id(USERNAME_TEXTBOX_ID), user);
 		setTextToFieldNoEnter(By.id(PASSWORD_TEXTBOX_ID), password);
-        clickOn(By.id(UNKOWN_LOCATION_ELEMENT_ID));	// TODO, not sure this belongs here because Mirebalais doesn't use it?, or at least make it optional? 
+		WebElement locations = findElement(By.id(LOCATIONS_ID));
+		WebElement firstLocation = locations.findElement(By.tagName("li"));	// choose the first location in the list
+		firstLocation.click();
 		clickOn(By.id(LOGIN_BUTTON_ID));
 		findElement(byFromHref(URL_ROOT + LOGOUT_PATH));	// this waits until the Logoff link is present
 	}
