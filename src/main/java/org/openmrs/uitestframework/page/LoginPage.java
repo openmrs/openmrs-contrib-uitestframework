11054f7de608df1f3076568f1d6ec1d78d6703c9
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-09-19 17:16:22 -0700

    RA-397 - Top-level administrative apps

--- src/main/java/org/openmrs/uitestframework/page/LoginPage.java
+++ src/main/java/org/openmrs/uitestframework/page/LoginPage.java
@@ -17,7 +17,9 @@ public class LoginPage extends AbstractBasePage {
 	static final String NURSE_PASSWORD = "Nurse123";
 	static final String DOCTOR_USERNAME = "doctor";
 	static final String DOCTOR_PASSWORD = "Doctor123";
-	
+	static final String SYSADMIN_USERNAME = "sysadmin";
+	static final String SYSADMIN_PASSWORD = "Sysadmin123";
+
 	private String UserName;
 	
 	private String Password;
@@ -59,5 +61,9 @@ public class LoginPage extends AbstractBasePage {
 
 	public void loginAsDoctor() {
 		login(DOCTOR_USERNAME, DOCTOR_PASSWORD);
+    }
+
+	public void loginAsSysadmin() {
+		login(SYSADMIN_USERNAME, SYSADMIN_PASSWORD);
     }
 }
