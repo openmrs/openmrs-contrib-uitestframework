2ae40672c1faba9df813a7b949e2bd1868ac3742
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-10-09 12:32:28 +0200

    Corrected waitForPageToBeReady

--- src/main/java/org/openmrs/uitestframework/page/AbstractBasePage.java
+++ src/main/java/org/openmrs/uitestframework/page/AbstractBasePage.java
@@ -16,8 +16,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.google.common.base.FinalizablePhantomReference;
-
 /**
  * A superclass for "real" pages. Has lots of handy methods for accessing
  * elements, clicking, filling fields. etc.
@@ -230,7 +228,7 @@ public abstract class AbstractBasePage implements Page {
 	            	if (domOnly) {
 	            		return domReady;
 	            	}
-	            	boolean pageReady = ((JavascriptExecutor)driver).executeScript("return pageReady").equals("true");
+	            	boolean pageReady = ((JavascriptExecutor)driver).executeScript("return pageReady").equals(true);
 	                return domReady && pageReady;
 	            }
 	        };
