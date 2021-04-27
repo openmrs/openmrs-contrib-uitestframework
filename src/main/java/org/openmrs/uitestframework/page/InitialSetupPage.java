45322395025458684c9997ed604f11de27dc6345
k-joseph <kaweesi@jembi.org>
2021-04-27 11:33:44 +0300

    Added InitialSetupPage for installation and upgrading

---
+++ src/main/java/org/openmrs/uitestframework/page/InitialSetupPage.java
@@ -0,0 +1,127 @@
+package org.openmrs.uitestframework.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class InitialSetupPage  extends Page {
+    private By rememberCheck = By.name("remember");
+    private By continueButton = By.name("continue");
+    private By passwordInput = By.cssSelector("input[type='password']");
+    private By remoteUrlInput = By.name("remoteUrl");
+    private By usernameInput = By.name("username");
+    private By errorMessage = By.className("openmrs_error");
+    protected TestProperties testProperties = TestProperties.instance();
+
+    public InitialSetupPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public String getPageUrl() {
+        return "/initialsetup";
+    }
+
+    private void pressContinue() {
+        clickOn(continueButton);
+        waitForPage();
+    }
+
+    private void clickOnInstallationType(Type type) {
+        clickOn(By.cssSelector("input[value='"+ type.name().toLowerCase() +"']"));
+    }
+
+    /**
+     * Installation Step 1
+     */
+    public void installationSelectLanguage() {
+        clickOn(rememberCheck);
+        pressContinue();
+    }
+
+
+    /**
+     * Installation Step 2
+     * @param type
+     */
+    public void selectInstallationType(Type type) {
+        clickOnInstallationType(type);
+        pressContinue();
+    }
+
+    /**
+     * Next installation steps after step 2 in right order
+     */
+    public void install(Type type) {
+        // enter password
+        if(type.equals(Type.SIMPLE)) {
+            setTextToFieldNoEnter(passwordInput, testProperties.getPassword());
+            pressContinue();// review
+            pressContinue();// review
+        } else if(type.equals(Type.ADVANCED)) {
+            setTextToFieldNoEnter(passwordInput, testProperties.getPassword());
+            pressContinue();
+            //step 2 of 5
+            driver.findElements(passwordInput).get(1).sendKeys(testProperties.getPassword());// last password field
+            pressContinue();
+
+            // step 3 of 5
+            pressContinue();
+
+            // step 4 of 5
+            driver.findElements(passwordInput).get(0).sendKeys(testProperties.getPassword());// password
+            driver.findElements(passwordInput).get(1).sendKeys(testProperties.getPassword());// confirm password
+            pressContinue();
+
+            // step 5 of 5
+            pressContinue();
+
+            pressContinue();// review
+        } else if(type.equals(Type.TESTING)) {
+            setTextToFieldNoEnter(remoteUrlInput, "http://demo.openmrs.org/openmrs");
+            enterUsernameAndPassword();
+            pressContinue();
+
+            // step 2 of 3
+            setTextToFieldNoEnter(passwordInput, testProperties.getPassword());
+            pressContinue();
+
+            //step 3 of 3
+            driver.findElements(passwordInput).get(1).sendKeys(testProperties.getPassword());
+            pressContinue();
+
+            waitForPageToLoad();
+
+            pressContinue();// review
+        }
+    }
+
+    public void enterUsernameAndPassword() {
+        setTextToFieldNoEnter(usernameInput, testProperties.getUsername());
+        setTextToFieldNoEnter(passwordInput, testProperties.getPassword());
+    }
+
+    /**
+     * Upgrading steps
+     */
+    public void upgrade() {
+        pressContinue();
+        pressContinue();
+    }
+
+    public enum Type {
+        SIMPLE, ADVANCED, TESTING
+    }
+
+    public void waitForSetupToComplete() {
+        waiter.until(ExpectedConditions.or(ExpectedConditions.urlContains("index.htm"), ExpectedConditions.urlContains("home.page")));
+    }
+
+    public String getErrorMessage() {
+        List<WebElement> error = getElementsIfExisting(errorMessage);
+        return error.size() > 0 ? error.get(0).getText() : null;
+    }
+}
