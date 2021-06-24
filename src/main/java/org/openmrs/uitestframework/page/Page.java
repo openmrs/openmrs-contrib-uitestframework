5f2b3f93cadef17c449b3f22a6a38e2c92ec3052
sherrif10 <sharfspace@gmail.com>
2021-06-24 11:48:02 +0300

    RATEST-92:RATEST-92:Failing Test: Contact Info Test

--- src/main/java/org/openmrs/uitestframework/page/Page.java
+++ src/main/java/org/openmrs/uitestframework/page/Page.java
@@ -362,7 +362,12 @@ public abstract class Page {
 
     public List<String> getValidationErrors() {
         List<String> validationErrors = new ArrayList<String>();
-        for (WebElement webElement : driver.findElements(By.className("error"))) {
+        for (WebElement webElement : driver.findElements(By.className("field-error"))) {
+            if (StringUtils.isNotBlank(webElement.getText())) {
+                validationErrors.add(webElement.getText());
+            }        
+        }
+        for(WebElement webElement: driver.findElements(By.className("error"))){
             if (StringUtils.isNotBlank(webElement.getText())) {
                 validationErrors.add(webElement.getText());
             }
